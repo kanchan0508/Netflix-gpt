6f8a645117125d11c153d88e1cfd33a14fe6ac69
kanchan gobari <gobarikanchan53@gmail.com>
2024-09-25 21:42:59 +0530

    Added Setup

---
+++ src/Component/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+        <Header />
+        <div>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Login
